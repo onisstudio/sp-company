9657a9ab9437763e488ee747cecf3d02d40bf17d
Bogdan Prăzniciuc <bogdan@onis.studio>
2020-06-07 14:22:52 +0100

    Code refactoring

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-import os, json
+import os
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,6 +9,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
 @app.route('/characters')
 def characters():
     data = []
@@ -15,23 +17,27 @@ def characters():
         data = json.load(json_data)
     return render_template("characters.html", page_title="Characters", characters=data)
 
+
+# character indivdual page, gets the character_name from json file
 @app.route('/characters/<character_name>')
 def about_member(character_name):
     character = {}
-    
+
     with open("data/characters.json", "r") as json_data:
         data = json.load(json_data)
         for obj in data:
             if obj["url"] == character_name:
                 character = obj
-    
+
     return render_template("character.html", character=character)
+
 
 @app.route('/about')
 def about():
     return render_template("about.html", page_title="About")
 
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=os.environ.get('PORT'),
-            debug=True)
+            debug=False)
