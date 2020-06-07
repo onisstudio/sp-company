5557297d17070359d08e29c35ec6931f05cf202b
Bogdan Prăzniciuc <bogdan@onis.studio>
2020-06-07 11:41:06 +0100

    Added bootstrap

--- run.py
+++ run.py
@@ -5,8 +5,16 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def index():
     return render_template("index.html")
+
+@app.route('/characters')
+def characters():
+    return render_template("characters.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
